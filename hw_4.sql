657b953c31be1f597cc58439886d2121ca904ae6
Sofiia Starokozheva <121119156+Starokozheva@users.noreply.github.com>
2023-11-22 00:08:55 +0200

    Add files via upload

---
+++ hw_4.sql
@@ -0,0 +1,45 @@
+-- Вывести название и стоимость в USD одного самого дорогого проданного товара
+
+SELECT
+   ProductName,
+   Price * 1.09 AS Price_usd
+FROM Products
+ORDER BY Price DESC
+LIMIT 1
+
+
+-- Вывести два самых дорогих товара из категории Beverages из USA
+
+SELECT *
+FROM Products
+
+WHERE
+   CategoryName = "Beverages"
+   AND
+   Country = "USA"  
+ORDER BY Price DESC
+LIMIT 2
+
+
+-- Удалить товары с ценой менее 50 EUR
+
+DELETE FROM Products
+WHERE
+Price <= 50
+
+
+-- Вывести список стран, которые поставляют морепродукты
+
+SELECT
+   Country
+FROM Suppliers
+WHERE
+CategoreName = "Seafood"
+
+
+-- Очистить поле ContactName у всех клиентов не из China
+
+UPDATE Customers
+   SET ContactName = ''
+WHERE
+NOT Country = "China"
